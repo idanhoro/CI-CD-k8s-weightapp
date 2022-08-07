b1e3e75be3bda5e984dcf0c1e85ae4e206d84700
Idan Horovitz <idanhh55@gmail.com>
2022-08-08 00:43:41 +0300

    Update README.md

--- Terraform-AKS/setup/main.tf
+++ Terraform-AKS/setup/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_container_registry" "container_registry" {
 
 # Create Kubernetes cluster Staging environment.
 module "k8s_staging" {
-  source = "../modules/kubernetes"
+  source = "../Terraform-AKS/modules/kubernetes"
   resource_group_location=var.resource_group_location
   kubernetes_cluster_name=var.staging_kubernetes_cluster_name
   container_registry_name=var.container_registry_name
@@ -25,7 +25,7 @@ module "k8s_staging" {
 
 # Create Kubernetes cluster Production environment
 module "k8s_production" {
-  source = "../modules/kubernetes"
+  source = "../Terraform-AKS/modules/kubernetes"
   resource_group_location=var.resource_group_location
   kubernetes_cluster_name=var.production_kubernetes_cluster_name
   container_registry_name=var.container_registry_name
